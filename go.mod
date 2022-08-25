033b2c2093aa66b8a7e43c6631ef83b75d555c9d
rojbar <55609284+rojbar@users.noreply.github.com>
2022-08-24 19:32:31 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rojbar/rftpic
+
+go 1.18
+
+require github.com/rojbar/rftpiu v0.0.0-20220825001733-cff1d4eaf04b
---
+++ rftpic.go
@@ -0,0 +1,113 @@
+package rftpic
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"os"
+
+	utils "github.com/rojbar/rftpiu"
+)
+
+/**
+	1. send a message informing that a file is going to be send
+	2. recieve confirmation and connection
+	3. initiate transfer
+	4. recieve confirmation all ok from server
+	5. end transmission either client or server
+	6. inform end user all ok
+**/
+func SendFile(port string, domain string, channel string, filePath string) error {
+	// inform the server we are gonna send a file and recieve a net.Conn to handle that
+	conn, res, errS := obtainConnection("SFTP > 1.0 ACTION: SEND \n SIZE: 12 EXTENSION: txt;", port, domain)
+	if errS != nil {
+		return errS
+	}
+	if res != "OK" {
+		return errors.New(res)
+	}
+	defer conn.Close()
+
+	file, errO := os.Open(filePath)
+	if errO != nil {
+		return errO
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(conn)
+	reader := bufio.NewReader(file)
+
+	buffer := make([]byte, 4096)
+	for {
+		_, errP := reader.Read(buffer)
+		if errP != nil {
+			if errP == io.EOF {
+				break
+			}
+			if errP != nil {
+				return errP
+			}
+		}
+
+		_, errW := writer.Write(buffer)
+		if errW != nil {
+			return errW
+		}
+		errF := writer.Flush()
+		if errF != nil {
+			return errF
+		}
+	}
+	fmt.Println("estoy aqui")
+	//here we check that server recieved file correctly
+	message, errMes := utils.ReadMessage(conn)
+	if errMes != nil {
+		print(errMes)
+		return errMes
+	}
+	fmt.Println(message)
+
+	// status, errSt := getStatus(response)
+	// if errSt != nil {
+	// 	return errSt
+	// }
+	// if status != "OK" {
+	// 	return errors.New(status)
+	// }
+
+	return nil
+}
+
+func Subscribe(message string, port string, domain string, channel string) error {
+	// conn, errS := send(message, port, domain) // here we tell the server that we want to create a subscription, the channel is in the payload, we recieve a net.Conn to handle the subscription
+	// // check(errS)
+	// go handleChannelSubscription(conn, channel)
+	return nil
+}
+
+// OK
+func obtainConnection(message string, port string, domain string) (net.Conn, string, error) {
+	conn, err := net.Dial("tcp", domain+":"+port)
+	if err != nil {
+		return nil, "", err
+	}
+
+	errW := utils.SendMessage(conn, message)
+	if errW != nil {
+		return nil, "", errW
+	}
+
+	response, errR := utils.ReadMessage(conn)
+	if errR != nil {
+		return nil, "", errR
+	}
+
+	//here we parse
+	status, errS := utils.GetKey(response, "STATUS")
+	if errS != nil {
+		return nil, "", errS
+	}
+	return conn, status, nil
+}
